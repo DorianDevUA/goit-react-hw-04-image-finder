9ba8b833ccbc292d9860ddfb694492cf62330044
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-09 13:48:16 +0400

    add ThemeProvider and GlobalStyle

---
+++ src/constants/theme.jsx
@@ -0,0 +1,12 @@
+const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    red: '#f44336',
+    green: '#4caf50',
+    blue: '#2196f3',
+  },
+  spacing: value => `${4 * value}px`,
+});
+
+export default theme;
