5f3b88e51a67b97dcd15aeecf9bca8364a60d873
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-09 13:10:54 +0400

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, quis.</p>
+    </>
+  );
+}
+
+export default App;
